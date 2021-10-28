54610ed5f488a37758a431cef05176501b581b52
ankitchoudhary87 <ankitchoudhary87@gmail.com>
2021-10-28 13:27:31 +0530

    Commit changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,7 @@ const Home = (props) => {
     console.log("clicked props", props);
     return (
         <div>
-            <h1>Home Component</h1>
+            <h1>Redux Example</h1>
             <div className="cart-wrapper">
                 <div className="img-wrapper item">
                     <img alt="iphone" src="https://www.freeiconspng.com/thumbs/iphone-x-pictures/apple-iphone-x-pictures-5.png" />
